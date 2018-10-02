31c0af35db89cdb4a97bba091accef14ab26f061
Roman Pirag <mr.anadale@yahoo.com>
2018-10-02 03:43:37 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+  
+  reversed = ""
+  sentence_split = sentence.split("")
+  sentence_split.each do |letter|
+  reversed = letter + reversed 
+  end
+
+end
+
+reverse_each_word("Happy Cakes I Like")
