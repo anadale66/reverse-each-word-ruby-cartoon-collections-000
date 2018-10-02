74b1b6c645469b493695991c6a3965967aa603e4
Roman Pirag <mr.anadale@yahoo.com>
2018-10-02 03:57:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,12 @@ def reverse_each_word(sentence)
   print sentence_split.join(" ")
 end
 
+def reverse_each_word(sentence)
+  sentence_split = sentence.split(" ")
+  
+  sentence_split.collect do |word| 
+    word.reverse!
+  end
+
+  print sentence_split.join(" ")
+end
