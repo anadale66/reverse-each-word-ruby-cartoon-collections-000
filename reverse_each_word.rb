d563220aa0b833c16c2b3810ee24f397084bdc04
Roman Pirag <mr.anadale@yahoo.com>
2018-10-02 04:21:09 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+  sentence_split = sentence.split(" ")
+  
+  sentence_split.each do |word| 
+    word.reverse!
+  end
+
+  return sentence_split.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence_split = sentence.split(" ")
+  
+  sentence_split.collect do |word| 
+    word.reverse!
+  end
+
+  return sentence_split.join(" ")
+end
